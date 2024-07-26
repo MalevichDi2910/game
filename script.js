52af5a6fc41df34d974e2d1521025b910c7aa750
MalevichDi2910 <malevicdiana@gmail.com>
2024-07-26 12:23:22 +0300

    feat: add new logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    let startingValue = 1;
+    let currentValue = 1;
     const maxValue = 10;
+    let rowPositionTop = 0;
 
     function multiply(a, b) {
         return a * b;
@@ -8,38 +9,70 @@ $(document).ready(function () {
 
     function addNextTask(a, b) {
         const task = `
-                    <div class="task-container" style="display: none;">
-                        <div>${a} x ${b} = <input type="number" class="result"></div>
-                    </div>
-                `;
+            <div class="task-container" style="display: none;">
+                ${a} x ${b} = <input type="number" class="result">
+            </div>
+        `;
         $('#task-container').append(task);
         $('#task-container .task-container').last().fadeIn(500);
         bindKeyup();
     }
 
+    function addCubes(count) {
+        const row = $('<div class="cube-row"></div>').css('top', rowPositionTop + 'px').appendTo('#cubes-container');
+
+        for (let i = 0; i < count; i++) {
+            row.append('<div class="cube animate"></div>');
+        }
+
+        rowPositionTop += 56;
+    }
+
+    function initializeTask() {
+        addNextTask(4, currentValue);
+        addCubes(4);
+        $('#done').prop('disabled', true);
+    }
+
+    function handleCorrectAnswer(val) {
+        $('.result').last().replaceWith(`<span>${val}</span>`);
+        $('#done').addClass('right');
+        setTimeout(() => {
+            $('#done').removeClass('right');
+        }, 500);
+
+        if (currentValue < maxValue) {
+            currentValue++;
+            addNextTask(4, currentValue);
+            addCubes(4);
+        } else {
+            resetGame();
+        }
+    }
+
+    function handleIncorrectAnswer() {
+        $('.result').last().addClass('wrong-input');
+        $('#done').removeClass('right').addClass('wrong');
+        setTimeout(() => {
+            $('.result').last().removeClass('wrong-input');
+            $('#done').removeClass('wrong');
+        }, 1000);
+    }
+
     function checkResultAndGoNext(val, a, b) {
         if (val == multiply(a, b)) {
-            $('.result').last().replaceWith(`<span>${val}</span>`);
-
-            $('#done').addClass('right');
-            setTimeout(() => {
-                $('#done').removeClass('right');
-            }, 500);
-
-            if (startingValue < maxValue) {
-                startingValue++;
-                const newB = startingValue;
-                addNextTask(4, newB);
-            }
-
+            handleCorrectAnswer(val);
         } else {
-            $('.result').last().addClass('wrong-input');
-            $('#done').removeClass('right').addClass('wrong');
-            setTimeout(() => {
-                $('.result').last().removeClass('wrong-input');
-                $('#done').removeClass('wrong');
-            }, 1000);
+            handleIncorrectAnswer();
         }
+    }
+
+    function resetGame() {
+        currentValue = 1;
+        rowPositionTop = 0;
+        $('#task-container').empty();
+        $('#cubes-container').empty();
+        initializeTask();
     }
 
     $('#done').on('click', function () {
@@ -54,12 +87,11 @@ $(document).ready(function () {
 
     function bindKeyup() {
         $('.result').off('keyup').on('keyup', function () {
-            const val = $(this).val();
-            const isValid = !isNaN(val) && val.trim() !== '';
+            const val = $(this).val().trim();
+            const isValid = !isNaN(val) && val !== '';
             $('#done').prop('disabled', !isValid);
         });
     }
 
-    addNextTask(4, startingValue);
-    $('#done').prop('disabled', true);
+    initializeTask();
 });
